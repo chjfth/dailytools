19406b165d0a29e6a1d98ba7f7c7379a22f0fd59
Jimm chja20 <chjfth@gmail.com>
2020-04-13 22:29:55 +0800

    Add code to report subprocess execution success or fail.

--- sleep-print/grab-output.py
+++ sleep-print/grab-output.py
@@ -10,7 +10,12 @@ subproc = subprocess.Popen(exe_prefix+'sleep-print *3 101 204 1040 1001 304 0',
 
 while True:
 	line = subproc.stdout.readline()
-	print(line.decode("utf8"), end="")
 	if not line:
 		break;
+	print(line.decode("utf8"), end="")
 
+subproc.wait() # we know that child process should have exited.
+print("Subprocess exit-code is: %d (%s)"%(
+	subproc.returncode, 
+	"success" if subproc.returncode==0 else "fail"
+	))
