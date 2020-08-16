0dd8daba0940f8b8df66cdd3411bc6035d6f2abe
Jimm chja20 <chjfth@gmail.com>
2020-08-16 08:46:33 +0800

    On chja20 Win10, I create a start-up shortcut to run rsync.bat after logon to my desktop.

---
+++ cmd-batch/rsync-daemon/wsl-chja20/rsyncd.bat
@@ -0,0 +1,13 @@
+@echo off
+@setlocal
+REM [2020-08-15] On chja20 Win10, I create a start-up shortcut to run this rsync daemon after logon to my desktop.
+
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set batdir_fs=%batdir:\=/%
+pushd %batdir%
+
+set MYCMD=C:\Windows\System32\wsl.exe rsync --daemon --port=1873 --config=rsyncd.conf --no-detach
+echo %MYCMD%
+%MYCMD%
+
