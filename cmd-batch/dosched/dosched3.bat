75f84a98e435f4c4c9eb5ca194526bcb5a779e3e
Jimm chja20 <chjfth@gmail.com>
2020-08-29 17:01:52 +0800

    add some comment

--- cmd-batch/dosched/dosched3.bat
+++ cmd-batch/dosched/dosched3.bat
@@ -1,1 +1,3 @@
+@REM Due to first param below is 3, all console output will be printed to CMD console, 
+@REM and before quitting, there is a 3 seconds delay before the CMD window vanishes.
 call %~dp0dosched_.bat 3 %*
--- cmd-batch/dosched/dosched9.bat
+++
@@ -1,1 +0,0 @@
-call %~dp0dosched_.bat 9 %*
