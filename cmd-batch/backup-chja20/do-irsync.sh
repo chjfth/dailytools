09ffe8f2703ff3336de9b3d5f0a75ad0fbb3df27
Jimm chja20 <chjfth@gmail.com>
2020-09-06 23:22:45 +0800

    Add --max-retry=3 to client-side do-irsync.sh .

--- cmd-batch/backup-chja20/do-irsync.sh
+++ cmd-batch/backup-chja20/do-irsync.sh
@@ -3,12 +3,14 @@
 export CHJHOST=10.22.3.84
 export PYTHONPATH=~/gitw/pyutils/pycode
 
-_irsync_="--datetime-pattern=YYYYMMDD --old-days=30"
+_irsync_="--datetime-pattern=YYYYMMDD --max-retry=3 --old-days=30"
 _rsync_="--rsync --progress --exclude-from=excludes.list"
 
 modlist=(myd myk mym) # define a Bash array
 	
 for mod in "${modlist[@]}"; do 
+	echo ""
+	echo "do-irsync.sh: Doing rsync://$CHJHOST:1873/$mod ..."
 	python3 -m cheese.incremental_rsync.irsync_cmd rsync://$CHJHOST:1873/$mod . $_irsync_ $IRSYNC_PARAMS $_rsync_ $RSYNC_PARAMS
 	if [ $? != 0 ]; then exit 4; fi
 done
---
+++ cmd-batch/backup-chja20/readme.txt
@@ -0,0 +1,4 @@
+chja20 is rsync-server that provides the source data to backup.
+
+do-irsync.bat and do-irsync.sh are run from a rsync-client machine that will store backup data of chja20.
+
