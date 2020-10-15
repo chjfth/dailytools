94396d0ba2e6bd3659488eae2893e0d51a54d25b
Jimm Chen crtek <chjfth@gmail.com>
2020-10-15 09:02:29 +0800

    Change parameter according to irsync's recent change.

--- cmd-batch/backup-confluence/do-irsync.sh
+++ cmd-batch/backup-confluence/do-irsync.sh
@@ -3,4 +3,4 @@ export RSYNC_PASSWORD=passcfu
 export TARGET_DIR=.
 
 export PYTHONPATH=~/gitw/pyutils/pycode
-python3 -m cheese.incremental_rsync.irsync_cmd "$RSYNC_URL" "$TARGET_DIR" --datetime-pattern=YYYYMMDD --old-days=30 --fetch-finish-dir-from=finishdir.txt --finish-dir-relative --rsync --progress
+python3 -m cheese.incremental_rsync.irsync_cmd "$RSYNC_URL" "$TARGET_DIR" --datetime-pattern=YYYYMMDD --old-days=30 --finish-dir-write-to=finishdir.txt --finish-dir-relative --rsync --progress
