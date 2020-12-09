e7b8fa2140ef2ff81b8c3b81cf7c4c24ac933488
JimmChen vbwin7 <chjfth@gmail.com>
2020-12-09 12:02:34 +0800

    count_dups implemented

--- pytools/csv-find-dup.py
+++ pytools/csv-find-dup.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
-import os, sys
+import os, sys, locale
 import argparse
 import csv
 
-def count_dups(fh):
+#print("sys.getdefaultencoding()=%s"%(sys.getdefaultencoding()))
+#print("locale.getpreferredencoding()=%s"%(locale.getpreferredencoding()))
+
+def count_dups(fh, verbose):
 	fh.seek(0)
 	reader = csv.reader(fh)
 	ireader = iter(reader)
 	
 	ar_headers = next(ireader)
 	count_fields = len(ar_headers)
+
+	# find longest header chars
+	max_header_width = max([len(x) for x in ar_headers])
+	max_header_prnwidth = min(max_header_width, 40)
 
 	ar_field_stats = [ { header_name : 1 } for header_name in ar_headers ]
 	
@@ -34,14 +41,20 @@ def count_dups(fh):
 
 	for idx_field, dict_dupcount in enumerate(ar_dupcount):
 		
+		fixed_width_fieldtext = "%-*.*s"%(max_header_prnwidth, max_header_prnwidth, 
+									ar_headers[idx_field])
+
 		dup_groups = len(dict_dupcount)
 		if dup_groups==0:
-			print("Field %d (%s) has no duplicate items."%(idx_field, ar_headers[idx_field]))
+			print("Field %d (%s) no duplicate items."%(idx_field, fixed_width_fieldtext))
 		else:
-			print("Field %d (%s) has %d group of duplicate items:"%(
-				idx_field, ar_headers[idx_field], dup_groups))
-			for dup_text, dup_count in dict_dupcount.items():
-				print("  (%d*) %s"%(dup_count, dup_text))
+			print("Field %d (%s) has %d group of duplicate items%s"%(
+				idx_field, fixed_width_fieldtext, dup_groups,
+				"." if not verbose else ":"
+				))
+			if verbose:
+				for dup_text, dup_count in dict_dupcount.items():
+					print("  (%d*) %s"%(dup_count, dup_text))
 
 	return 0
 	
@@ -73,7 +86,7 @@ def my_parse_args():
 			' * verbose mode as well list #line of those duplicates.\n'
 	)
 
-	ap.add_argument('-e', '--encoding', type=str,
+	ap.add_argument('-e', '--encoding', type=str, default='',
 		help='Assign text encoding of the input csv file. If omitted, system default will be used.\n'
 			'Typical encodings: utf8, gbk, big5, utf16le.'
 	)
@@ -82,7 +95,7 @@ def my_parse_args():
 		# If no command-line parameter given, display help and quit.
 		print("You must provide a csv filename.")
 		print("To see full program options, run:\n")
-		print("  %s --help"%(__file__))
+		print("  %s --help"%(os.path.basename(__file__)))
 		exit(1)
 
 	if sys.argv[1]=='--help':
@@ -94,14 +107,19 @@ def my_parse_args():
 	return args
 
 if __name__=="__main__":
-
-	
-	
-	
 	args = my_parse_args()
 
-	csvfile = sys.argv[1]
-	fh = open(csvfile, "r")
-	ret = count_dups(fh)
+	csvfile = args.csv_filename
+	headers_to_chk = args.header
+	text_encoding = locale.getpreferredencoding() if not args.encoding else args.encoding
+	verbose = 0 if not args.verbose else 1
+
+	fh = open(csvfile, "r", encoding=text_encoding)
+	
+	if headers_to_chk==None:
+		ret = count_dups(fh, verbose)
+	else:
+		ret = 4
+
 	exit(ret)
 
