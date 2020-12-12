e04451249a83bf014350bb58a42f53fc4cc284e7
JimmChen vbwin7 <chjfth@gmail.com>
2020-12-12 09:26:44 +0800

    simplify code

--- pytools/err-tell/err-tell.py
+++ pytools/err-tell/err-tell.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
+# See weird behavior on Python 3.5~3.7...
+
 import os, sys, locale
-import csv
+
+g_use_ftell = True
 
 def enum_csv_rows_by_filehandle(fh):
 	while True:
-		filepos = fh.tell() # DANGEROUS?!
+		
+		if g_use_ftell:
+			filepos = fh.tell() # tell() is DANGEROUS!!
+		else:
+			filepos = -1
+		
 		linetext = fh.readline()
 		
 		if linetext=='':
 			return # meet end of file
 		
-		csv_row = next( csv.reader([linetext]) )
-		# -- csv_row is a list of celt.
-		yield filepos, csv_row 
+		yield filepos, linetext
 
 def main(csv_filename):
 
 	fh = open(csv_filename, 'r', encoding='gbk')
 
-	fh.seek(0)
 	csv_spliter = enum_csv_rows_by_filehandle(fh)
 	
 	for idx_line, (ofs, row) in enumerate(csv_spliter):
@@ -28,12 +33,16 @@ def main(csv_filename):
 
 
 if __name__=="__main__":
-	
+
+	global g_use_ftell
 	csv_filename = 'err-tell-gbk.csv'
 	
 	if len(sys.argv)>1:
 		print("Using a csv filename %s."%(sys.argv[1]))
 		csv_filename = sys.argv[1]
+	
+	if len(sys.argv)>2 and sys.argv[2]=="0":
+		g_use_ftell = False
 		
 	main(csv_filename)
 	
