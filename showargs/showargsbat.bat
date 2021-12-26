f2ea832ca2da561e58aceb2b38891192c46d7e5e
chj Win7Evernote VM <chjfth@gmail.com>
2021-12-26 12:14:11 +0800

    showargsbat.bat : dumps each parameters(%1 %2 %3 ...) seen within this bat itself

---
+++ showargs/showargsbat.bat
@@ -0,0 +1,85 @@
+@echo off
+set local
+REM This bat dumps each parameters(%1 %2 %3 ...) seen within this bat itself.
+REM -- Jimm Chen 2021.12.16
+
+echo [0] %0
+echo [~0] %~0
+set n=1
+
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
+shift
+set /a n=n+1
+if [%1]==[] exit /b
+echo  [%n%] %1
+echo [~%n%] %~1
+
