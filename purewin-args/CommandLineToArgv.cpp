1ee9ce8c94a439498235ec28855f1a8d8da19ed4
chj Win7Evernote VM <chjfth@gmail.com>
2021-12-27 20:36:36 +0800

    Program rename to CommandLineToArgv so to be more explicit.

--- purewin-args/CommandLineToArgv.cpp
+++ purewin-args/CommandLineToArgv.cpp
@@ -1,10 +1,10 @@
 /* 2021.10.18 Jimm Chen
 
-purwin-args.cpp, show Windows API CommandLineToArgvW()'s behaivor.
+CommandLineToArgv.cpp, show Windows API CommandLineToArgvW()'s behaivor.
 
 Compile it with VC2010, not linking any MSVCRT libraries, resulting exe is 2560 bytes.
 
-	cl purewin-args.cpp /UNICODE /GS- /link /nodefaultlib /subsystem:console kernel32.lib shell32.lib user32.lib
+	cl CommandLineToArgv.cpp /UNICODE /GS- /link /nodefaultlib /subsystem:console kernel32.lib shell32.lib user32.lib
 
 We'll see that CommandLineToArgvW's behavior is not the same as that of main()'s argv[].
 What's more, it is NOT the same as cmd's command line parsing into %1, %2, %3 etc.
@@ -12,7 +12,7 @@ So, I think CommandLineToArgvW is quite useless in real world.
 
 You may try it with:
 
-	purewin-args.exe C:\1st-param """Debug ANSI"" 2.2-param" 3rd-param
+	CommandLineToArgv.exe C:\1st-param """Debug ANSI"" 2.2-param" 3rd-param
 
 */
 
