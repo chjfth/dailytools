9aa7192dcf588323cdb9e3be213678aba9bc1b2d
chj Win7Evernote VM <chjfth@gmail.com>
2021-12-27 21:33:24 +0800

    Increase local string buffer to 4000 chars, now we need to link chkstk.obj . Print raw GetCommandLineW() returned chars.

--- CommandLineToArgv/CommandLineToArgv.cpp
+++ CommandLineToArgv/CommandLineToArgv.cpp
@@ -4,7 +4,7 @@ CommandLineToArgv.cpp, show Windows API CommandLineToArgvW()'s behaivor.
 
 Compile it with VC2010, not linking any MSVCRT libraries, resulting exe is 2560 bytes.
 
-	cl CommandLineToArgv.cpp /UNICODE /GS- /link /nodefaultlib /subsystem:console kernel32.lib shell32.lib user32.lib
+	cl CommandLineToArgv.cpp /UNICODE /GS- /link /nodefaultlib /subsystem:console chkstk.obj kernel32.lib shell32.lib user32.lib
 
 We'll see that CommandLineToArgvW's behavior is not the same as that of main()'s argv[].
 What's more, it is NOT the same as cmd's command line parsing into %1, %2, %3 etc.
@@ -23,17 +23,19 @@ You may try it with:
 int __stdcall mainCRTStartup()
 {
 	DWORD nWritten = 0;
-	WCHAR tbuf[1000];
+	WCHAR tbuf[4000];
 
 	LPWSTR pszRawCmdline = GetCommandLineW();
 	HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	const WCHAR *hint = L"Raw string from GetCommandLineW():\n";
-	WriteConsoleW(hout, hint, lstrlenW(hint), &nWritten, NULL);
-	WriteConsoleW(hout, pszRawCmdline, lstrlenW(pszRawCmdline), &nWritten, NULL);
+	int nRawChars = lstrlenW(pszRawCmdline);
+	wsprintfW(tbuf, L"(%d chars)Raw string from GetCommandLineW():\n", nRawChars);
+	WriteConsoleW(hout, tbuf, lstrlenW(tbuf), &nWritten, NULL);
+	
+	WriteConsoleW(hout, pszRawCmdline, nRawChars, &nWritten, NULL);
 	WriteConsoleW(hout, L"\n\n", 2, &nWritten, NULL);
 	
-	hint = L"Result from CommandLineToArgvW():\n";
+	const WCHAR *hint = L"Result from CommandLineToArgvW():\n";
 	WriteConsoleW(hout, hint, lstrlenW(hint), &nWritten, NULL);
 	
 	int numArgs = 0;
