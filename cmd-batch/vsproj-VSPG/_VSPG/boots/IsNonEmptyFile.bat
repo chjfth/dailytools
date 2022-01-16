9f56dfdaa6410f2e3e738dab8a6b1710709782ed
chj Win7Evernote VM <chjfth@gmail.com>
2022-01-16 09:39:17 +0800

    Update IsNonEmptyFile.bat

--- cmd-batch/vsproj-VSPG/_VSPG/boots/IsNonEmptyFile.bat
+++ cmd-batch/vsproj-VSPG/_VSPG/boots/IsNonEmptyFile.bat
@@ -15,13 +15,14 @@ if "%infile%" == "" (
 	exit /b 4
 )
 
-REM Check that it is not a folder.
-REM Thanks: https://stackoverflow.com/a/138995/151453
-
-FOR %%i IN ("%infile%") DO IF EXIST %%~si\NUL (
+call "%batdir%\IsFolder.bat" "%infile%"
+if not errorlevel 1 (
 	call :Echos [ERROR] Input path "%infile%" is a directory. I need a file.
 	exit /b 4
 )
+
+REM Must reset ERRORLEVEL here(could have been set by IsFolder.bat)
+call :SetErrorlevel 0
 
 set size=0
 for /f delims^=^ eol^= %%i in ("%infile%") do (
@@ -37,9 +38,9 @@ if "%size%" == "" (
 if "%size%" == "0" (
 	REM Zero byte output, something must gone wrong.
 	exit /b 4
-) else (
-	exit /b 0
-)
+) 
+
+exit /b 0
 
 
 REM =============================
@@ -53,3 +54,8 @@ exit /b 0
 :EchoExec
   echo %_vspgINDENTS%[%batfilenam%] EXEC: %*
 exit /b 0
+
+:SetErrorlevel
+  REM Usage example:
+  REM call :SetErrorlevel 4
+exit /b %1
