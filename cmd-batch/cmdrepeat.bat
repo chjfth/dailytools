5b8d43c4cee5bad765c807c9d3ec13580d5d80fd
Jimm chja20 <chjfth@gmail.com>
2022-03-06 14:58:40 +0800

    cmdrepeat.bat : Repeat executing a command.

---
+++ cmd-batch/cmdrepeat.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal EnableDelayedExpansion
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+
+set inputcmd=%*
+set param1=%~1
+
+if "%param1%" == "" (
+	echo Missing input command.
+	exit /b 4
+)
+
+set /A ii=0
+
+:REPEAT
+
+set /A ii=%ii%+1
+echo [REPEAT %ii%] Now %DATE% %TIME%
+
+%*
+
+set exitcode=%ERRORLEVEL%
+
+timeout /t 3
+
+goto :REPEAT
+
+exit /b %ERRORLEVEL%
