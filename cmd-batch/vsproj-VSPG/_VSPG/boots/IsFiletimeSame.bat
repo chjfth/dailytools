fab083187b609b7dae759bc47c4115837bec49ef
Jimm Chen WDK10 <chjfth@gmail.com>
2022-03-14 18:58:49 +0800

    PostBuild-CopyOutput4.bat improvement: Wrap calling of vspg_copy1file.bat inside LoopExecUntilSucc.bat, so much more reliable copy error recovery.
    version 299.

---
+++ cmd-batch/vsproj-VSPG/_VSPG/boots/IsFiletimeSame.bat
@@ -0,0 +1,76 @@
+@echo off
+setlocal EnableDelayedExpansion
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set _vspgINDENTS=%_vspgINDENTS%.
+
+: This is a function.
+
+:IsFiletimeSame
+REM Usage: 
+REM call IsFiletimeSame.bat filepath1 filepath2
+REM
+REM 
+REM 
+REM exit 0 if filetime same, otherwise, non-zero exit-code.
+REM
+REM Implementation note: 
+REM * forfiles can report file modification time as accurate as a second.
+REM * forfiles can check only file/file-pattern in current directory, so we need to pushd first.
+
+set filepath1=%~1
+set filepath2=%~2
+
+if "%filepath1%" == "" (
+	call :Echos [Error] Missing parameter 1
+	exit /b 4
+)
+
+if "%filepath2%" == "" (
+	call :Echos [Error] Missing parameter 2
+	exit /b 4
+)
+
+if not exist "%filepath2%" (
+	exit /b 4
+)
+
+call "%batdir%\PathSplit.bat" "%filepath1%" dir1 filenam1
+pushd "%dir1%"
+for /F "tokens=*" %%a in ('forfiles /m "%filenam1%" /c "cmd.exe /c echo @fdate @ftime"') do set filetime1=%%a
+if errorlevel 1 exit /b 4
+popd
+
+call "%batdir%\PathSplit.bat" "%filepath2%" dir2 filenam2
+pushd "%dir2%"
+for /F "tokens=*" %%a in ('forfiles /m "%filenam2%" /c "cmd.exe /c echo @fdate @ftime"') do set filetime2=%%a
+if errorlevel 1 exit /b 4
+popd
+
+if "%filetime1%" == "" (
+	call :Echos [UNEXPECT] filetime1 is empty string. ("%filepath1%")
+	exit /b 4
+)
+
+if "%filetime2%" == "" (
+	call :Echos [UNEXPECT] filetime2 is empty string. ("%filepath2%")
+	exit /b 4
+)
+
+if "%filetime1%" == "%filetime2%" (
+	exit /b 0
+) else (
+	exit /b 4
+)
+
+exit /b 0
+
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+:Echos
+  echo %_vspgINDENTS%[%batfilenam%] %*
+exit /b 0
---
+++ cmd-batch/vsproj-VSPG/_VSPG/boots/LoopExecUntilSucc.bat
@@ -0,0 +1,84 @@
+@echo off
+setlocal EnableDelayedExpansion
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set _vspgINDENTS=%_vspgINDENTS%.
+
+: This is a function.
+
+:LoopExecUntilSucc
+REM Usage: 
+REM call LoopExec.bat #10# commands params ...
+REM
+REM The first param determines how many times to loop.
+REM On first success(exitcode=0), this bat quits.
+REM 
+
+set param1=%~1
+set charhead=%param1:~0,1%
+set chartail=%param1:~-1%
+
+if "%param1%" == "" (
+	call :Echos [ERROR] Missing first parameter as loop-count.
+	exit /b 4
+)
+
+if "%~2" == "" (
+	call :Echos [ERROR] Missing parameter: empty command to execute.
+	exit /b 4
+)
+
+if not "%charhead%" == "#" (
+	call :Echos [ERROR] First char of first parameter is not #
+	exit /b 4
+)
+if not "%chartail%" == "#" (
+	call :Echos [ERROR] Final char of first parameter is not #
+	exit /b 4
+)
+
+set LoopCount=%param1:~1,-1%
+
+set _LoopCount_=#%LoopCount%#
+set cmdexec=%*
+
+for %%g in (%cmdexec%) do set cmdexec=!cmdexec:%_LoopCount_%=!
+
+REM -- Now we have stripped away %1 from the passed-in command line,
+REM %cmdexec% is the very command to execute.
+
+set /a NowCount=1
+
+:Loop
+
+call :EchoAndExec1 %cmdexec%
+if not errorlevel 1 (
+	exit /b 0
+)
+
+if "%NowCount%" == "%LoopCount%" exit /b 4
+
+set /a NowCount=%NowCount%+1
+
+call :Echos Will retry in 2 seconds...
+call "%batdir%\DelaySeconds.bat" 2
+
+goto :Loop
+
+
+exit /b 0
+
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+:Echos
+  echo %_vspgINDENTS%[%batfilenam%] %*
+exit /b 0
+
+:EchoAndExec1
+  echo %_vspgINDENTS%[%batfilenam%] %NowCount%/%LoopCount% EXEC: %*
+  call %*
+exit /b %ERRORLEVEL%
