634704ee6a5b87d01f73318f4e08897d58cab445
Jimm Chen WDK10 <chjfth@gmail.com>
2022-03-25 12:56:41 +0800

    update DelOneFile.bat

--- cmd-batch/vsproj-VSPG/_VSPG/boots/DelOneFile.bat
+++ cmd-batch/vsproj-VSPG/_VSPG/boots/DelOneFile.bat
@@ -1,15 +1,50 @@
+@echo off
 setlocal EnableDelayedExpansion
 
-: This is a function.
-: It deletes a file and correctly reports ERRORLEVEL
-: Thanks to: https://stackoverflow.com/a/33403497/151453
+set batfilenam=%~n0%~x0
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set _vspgINDENTS=%_vspgINDENTS%.
+REM call :Echos START from %batdir%
 
-:DeleteOneFile
+: This is a function.
+:DelOneFile
 
 : Param1: the filepath to delete
 
-if not exist "%~1" exit /b 0
+if not exist "%~1" (
+	call :Echos Already deleted: "%~1"
+	exit /b 0
+)
+
+call :Echos del "%~1"
+
+: Now delete a file and correctly reports ERRORLEVEL.
+: Thanks to: https://stackoverflow.com/a/33403497/151453
 
 > nul ver & for /F "tokens=*" %%# in ('del /Q "%~1" 2^>^&1 1^> nul') do (2> nul set =)
 
 exit /b %ERRORLEVEL%
+
+
+
+REM =============================
+REM ====== Functions Below ======
+REM =============================
+
+:Echos
+  REM This function preserves %ERRORLEVEL% for the caller,
+  REM and, LastError does NOT pollute the caller.
+  setlocal & set LastError=%ERRORLEVEL%
+  echo %_vspgINDENTS%[%batfilenam%] %*
+exit /b %LastError%
+
+:EchoAndExec
+  echo %_vspgINDENTS%[%batfilenam%] EXEC: %*
+  %*
+exit /b %ERRORLEVEL%
+
+:EchoVar
+  setlocal & set Varname=%~1
+  call echo %_vspgINDENTS%[%batfilenam%] %Varname% = %%%Varname%%%
+exit /b 0
