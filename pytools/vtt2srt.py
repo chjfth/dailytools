074360cc553b3668ffc28b7362bb2833f305e36e
Jimm chja20 <chjfth@gmail.com>
2022-05-22 14:22:31 +0800

    Fix: Now vtt2srt.py can process a subtitle with multiple text lines.

--- pytools/vtt2srt.py
+++ pytools/vtt2srt.py
@@ -3,6 +3,13 @@
 
 import re, os, sys
 
+def unquote_html_entity(s):
+	s = s.replace('&nbsp;', ' ')
+	s = s.replace('&lt', '<')
+	s = s.replace('&gt', '>')
+	return s
+
+
 if len(sys.argv)==1:
 	print("Need a .vtt filename as input.")
 	exit(1)
@@ -13,23 +20,31 @@ outfile = os.path.splitext(infile)[0]+".srt"
 count = 0
 with open(infile, "r", encoding='utf8') as infh, \
 		open(outfile, "w", encoding='utf8') as outfh:
+
 	while True:
 		line = infh.readline()
-		if line=='':
+		
+		if not line:
 			break
+		
 		m = re.match(r'([0-9\:]+)\.([0-9]{3}) --> ([0-9\:]+)\.([0-9]{3})', line)
-		if not m:
+		
+		if m:
+			# Got a time-range line
+			count += 1
+			srtsect = "%d\n"%(count)
+			srtsect += "{},{} --> {},{}\n".format(m.group(1), m.group(2), m.group(3), m.group(4))
+			outfh.write(srtsect)
 			continue
 		
-		subtitle_text = infh.readline()
-		
-		#print("%s"%(m.group(0)))
+		if count==0:
+			# ignore all vtt text before first subtitle line
+			continue
 
-		count += 1
-		srtsect = "%d\n"%(count)
-		srtsect += "{},{} --> {},{}\n".format(m.group(1), m.group(2), m.group(3), m.group(4))
-		srtsect += subtitle_text+"\n"
-		
-		outfh.write(srtsect)
+		# This is subtitle line.
+		# Replace each unquote html entity reference
+		line = unquote_html_entity(line)
+		outfh.write(line)
+	
 
 print("Done: %s"%(outfile))
