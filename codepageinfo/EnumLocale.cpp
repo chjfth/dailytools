b4d377efdf597ee30fec330430c4db901031dae0
chj Win7Evernote VM <chjfth@gmail.com>
2022-08-26 18:58:57 +0800

    EnumLocale.exe : User can filter LOCALE_SPECIFICDATA or LOCALE_NEUTRALDATA, from command line or interactively.

--- codepageinfo/EnumLocale.cpp
+++ codepageinfo/EnumLocale.cpp
@@ -26,16 +26,39 @@ void my_tprintf(const TCHAR *szfmt, ...)
 	fflush(stdout); // important
 }
 
+int my_getch_noblock()
+{
+	if(_isatty(_fileno(stdout)))
+	{
+		int key = _getch();
+		return key;
+	}
+	else 
+		return '0';		
+}
+
+enum Filter_et
+{
+	Filter_None = 0,
+	Filter_LangRegn = 1,    // need only <lang>-<REGION> locales
+	Filter_Neutral = 2, // need only neutral locales
+};
+
 struct EnumInfo_t
 {
+	int callbacks;
 	int count;
 	int empty;
+	Filter_et filter;
 };
+
 
 BOOL CALLBACK EnumLocalesProcEx(LPWSTR lpLocaleString,  DWORD dwFlags, LPARAM lParam)
 {
+	int &callbacks = ((EnumInfo_t*)lParam)->callbacks;
+	callbacks++;
+
 	int &count = ((EnumInfo_t*)lParam)->count;
-	count++;
 
 	if(!lpLocaleString || !lpLocaleString[0])
 	{
@@ -76,13 +99,23 @@ BOOL CALLBACK EnumLocalesProcEx(LPWSTR lpLocaleString,  DWORD dwFlags, LPARAM lP
 			exflags[slen-2] = _T('\0');
 	}
 
-	my_tprintf(_T("[%d] %s ; %s @ %s ; LCID=%s"), count, lpLocaleString, szLang, szRegn, szLCID);
-	//
-	if(exflags[0])
+	const Filter_et &filter = ((EnumInfo_t*)lParam)->filter;
+
+	if(filter==Filter_None 
+		|| ((filter==Filter_LangRegn) && (dwFlags&LOCALE_SPECIFICDATA))
+		|| ((filter==Filter_Neutral) && (dwFlags&LOCALE_NEUTRALDATA)) 
+		)
 	{
-		my_tprintf(_T(" (%s)"), exflags);
+		count++;
+
+		my_tprintf(_T("[%d] %s ; %s @ %s ; LCID=%s"), count, lpLocaleString, szLang, szRegn, szLCID);
+		//
+		if(exflags[0])
+		{
+			my_tprintf(_T(" (%s)"), exflags);
+		}
+		my_tprintf(_T("\n"));
 	}
-	my_tprintf(_T("\n"));
 
 	// TEST "localized" names. Why still get English text?
 	GetLocaleInfoEx(lpLocaleString, LOCALE_SLOCALIZEDLANGUAGENAME, szLang, ARRAYSIZE(szLang));
@@ -102,10 +135,10 @@ int AskUserForFlags()
 	my_tprintf(_T("[2] LOCALE_SUPPLEMENTAL\n"));
 	my_tprintf(_T("[4] LOCALE_ALTERNATE_SORTS\n"));
 	my_tprintf(_T("Select: "));
-	int key = _getch();
+	int key = my_getch_noblock();
 	int num = key - '0';
 	if(num>=0 && num<=7)
-		;
+		; // valid input
 	else
 		num = 0;
 
@@ -113,17 +146,52 @@ int AskUserForFlags()
 	return num;
 }
 
+Filter_et AskForFilters()
+{
+	my_tprintf(_T("Select filter for LOCALE_WINDOWS\n"));
+	my_tprintf(_T("[0] Show all\n"));
+	my_tprintf(_T("[1] Show only <lang>-<REGION> entries (LOCALE_SPECIFICDATA)\n"));
+	my_tprintf(_T("[2] Show only neutral entries (LOCALE_NEUTRALDATA)\n"));
+	my_tprintf(_T("Select: "));
+	int key = my_getch_noblock();
+	int num = key - '0';
+	if(num>=0 && num<=2)
+		; // valid input
+	else
+		num = 0;
+
+	my_tprintf(_T("%d\n"), num);
+	return (Filter_et)num;
+}
+
 int _tmain(int argc, TCHAR *argv[])
 {
+	// Param1: dwFlags passed to EnumSystemLocalesEx().
+	// If omit, select interactively.
+	// 
+	// Param2: Filter the entries enumerated. User can choose display
+	// only <lang>-<REGION> locales, or *neutral* locales.
+	// If omit, select interactively.
+
 	_tsetlocale(LC_CTYPE, _T(""));
 
+	EnumInfo_t exi = {};
+
 	int flags = 0;
-	if(argc==1 || (flags=_ttoi(argv[1]))<0)
+	if(argc<=1 || (flags=_ttoi(argv[1]))<0)
 	{
 		flags = AskUserForFlags();
 	}
 
-	EnumInfo_t exi = {};
+	if(flags & LOCALE_WINDOWS)
+	{
+		// We only apply filter to LOCALE_WINDOWS, bcz, for LOCALE_ALTERNATE_SORTS,
+		// neither LOCALE_SPECIFICDATA or LOCALE_NEUTRALDATA is seen from the callback.
+		if(argc<=2 || (exi.filter=(Filter_et)_ttoi(argv[2]))<0)
+		{
+			exi.filter = AskForFilters();
+		}
+	}
 
 	BOOL succ = EnumSystemLocalesEx(EnumLocalesProcEx, flags, (LPARAM)&exi, 0);
 	if(succ)
@@ -132,7 +200,8 @@ int _tmain(int argc, TCHAR *argv[])
 			my_tprintf(_T("None.\n"));
 		
 		if(exi.empty>0)
-			my_tprintf(_T("NOTE: There are %d empty locale-name given by EnumSystemLocalesEx().\n"), exi.empty);
+			my_tprintf(_T("Callbacks:%d , Shown:%d, and %d empty-string given by EnumSystemLocalesEx().\n"), 
+				exi.callbacks, exi.count, exi.empty);
 	}
 	else
 	{
