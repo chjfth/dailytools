6f0ff150bba1d97fbbca6ddbef933f19063cfcd1
Jimm chja20 <chjfth@gmail.com>
2024-02-01 10:04:37 +0800

    Update to prev: Cope with PANO_xxx.jpg
    Hmm, today with M11U, q96 -> q80, the filesize is reduced to 30~40%.

--- pytools/MiPhone-jpg80q.py
+++ pytools/MiPhone-jpg80q.py
@@ -11,17 +11,26 @@ from PIL import Image
 
 pyfilename = os.path.split(sys.argv[0])[1]
 
-def jpgfn_need_process(jpgfn):
-	m = re.match(r"IMG_[0-9]{8}_[0-9]{6}(.+).jpg", jpgfn)
+def jpgfn_get_newname(jpgfn):
+
+	# Note: For PANO_yyyymmdd_hhmmss...jpg, I will rename it to IMG_yyyymmdd_hhmmss_pano...jpg
+	# so that when the filenames are sorted alphabetically, they natuarally appears timestamp sorted.
+
+	m = re.match(r"(IMG|PANO)(_[0-9]{8}_[0-9]{6})(.+).jpg", jpgfn)
 	if m:
-		suffix = m.group(1)
+		prefix = m.group(1)
+		midpart = m.group(2)
+		suffix = m.group(3)
 		if re.match(r"_[0-9]+", suffix):
 			# This is from burst-shot duplicates; discard it.
-			return False
+			return None
 		else:
-			return True
+			if(prefix=="IMG"):
+				return jpgfn
+			else:
+				return "IMG" + midpart + "_pano" + suffix + ".jpg"
 	else:
-		return False
+		return None
 
 if __name__=='__main__':
 	if len(sys.argv)<3:
@@ -31,6 +40,8 @@ if __name__=='__main__':
 		print("Files with the following pattern, will be re-encoded to be 80% jpg quality.")
 		print("    IMG_20231107_184622 xxx.jpg")
 		print("    IMG_20231107_184622_xxx.jpg")
+		print("    PANO_20231107_184622 xxx.jpg")
+		print("    PANO_20231107_184622_xxx.jpg")
 		print("")
 		print("Files like IMG_20231107_184622.jpg will be left as is.")
 		exit(1)
@@ -48,22 +59,31 @@ if __name__=='__main__':
 	nencode = 0
 
 	for jpgfn in os.listdir(indir):
-		if not jpgfn_need_process(jpgfn):
+		jpgfn_new = jpgfn_get_newname(jpgfn)
+		if not jpgfn_new:
 			continue
 
 		oldjpgpath = os.path.join(indir, jpgfn)
-		newjpgpath = os.path.join(outdir, jpgfn)
+		newjpgpath = os.path.join(outdir, jpgfn_new)
 
 		if os.path.exists(newjpgpath):
 			print("Skip existing %s"%(newjpgpath))
 			nskip += 1
 			continue
 
-		print("Encoding %s"%(newjpgpath))
+		print("Encoding to %s"%(newjpgpath))
 
 		with Image.open(oldjpgpath) as img:
 			img.save(newjpgpath, quality=80)
 
 		nencode += 1
+
+		oldsize = os.path.getsize(oldjpgpath)
+		newsize = os.path.getsize(newjpgpath)
+		print("    %.1f MB -> %.1f MB (reduced to %d%%)"%(
+			oldsize/(1024*1024),
+			newsize/(1024*1024) ,
+			newsize*100/oldsize
+		))
 
 	print("Done, skip %d + re-encode %d, total %d"%(nskip, nencode, nskip+nencode))
