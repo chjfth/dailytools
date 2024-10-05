805723e99652f5dc6619c2d8517df688dff9eab1
Jimm chja20 <chjfth@gmail.com>
2024-10-05 18:41:29 +0800

    Use requests.Session(), to enable HTTP keep-alive.

--- pytools/http-sequence-get.py
+++ pytools/http-sequence-get.py
@@ -2,43 +2,56 @@ import os, sys, time
 import requests
 
 RANGE_START = 1
-RANGE_MAX = 20
+RANGE_MAX = 890
 
 # Base URL for the files
 base_url = "https://cfvod.kaltura.com/hls/p/2503451/sp/0/serveFlavor/entryId/1_2ctpww6v/v/11/ev/7/flavorId/1_jwmhznmn/name/a.mp4/" # video and audio
-#base_url ="https://cfvod.kaltura.com/hls/p/2503451/sp/0/serveFlavor/entryId/1_2ctpww6v/v/21/ev/7/flavorId/1_ly4l9p05/name/a.mp4/" # audio only
 filename_ptn = "seg-%d-v1-a1.ts"
 
 print("Base URL: " + base_url)
 print("filename pattern: " + filename_ptn)
 
-for i in range(RANGE_START, RANGE_MAX+1):
-    
-    filename = filename_ptn%(i)
-    
-    if os.path.exists(filename):
-    	continue
-    
-    file_url = base_url + filename
 
-    while True:
-	    try:
-	        # Send a GET request to download the file
-	        response = requests.get(file_url)
-	        response.raise_for_status()  # Check for HTTP errors
+def do_http_downloads(istart, imax):
+	
+	# We need HTTP keep-alive feature, so use a "session".
+	with requests.Session() as session:
 
-	        # Save the file to the local directory
-	        with open(filename, 'wb') as file:
-	            file.write(response.content)
-	        
-	        print(f"Downloaded: " + filename)
-	        break
-	    
-	    except requests.exceptions.RequestException as e:
-	        print(f"Error an retrying: " + filename)
-	        time.sleep(2)
+		for i in range(istart, imax+1):
 
-print("Done %s to %s ."%( 
-	filename_ptn%(RANGE_START),
-	filename_ptn%(RANGE_MAX)
-	))
+			filename = filename_ptn%(i)
+
+			if os.path.exists(filename):
+				continue
+
+			file_url = base_url + filename
+
+			try:
+				# Send a GET request to download the file
+				response = session.get(file_url)
+				response.raise_for_status()  # Check for HTTP errors
+			except requests.exceptions.RequestException as e:
+				print(f"HTTP error and retrying: " + filename)
+				raise
+
+			# Save the file to the local directory
+			with open(filename, 'wb') as file:
+				file.write(response.content)
+			
+			print(f"Downloaded: " + filename)
+
+
+if __name__=='__main__':
+	
+	while True:
+		try:
+			do_http_downloads(RANGE_START, RANGE_MAX)
+
+		except requests.exceptions.RequestException as e:
+			time.sleep(1)
+			continue
+	
+	print("Done %s to %s ."%( 
+		filename_ptn%(RANGE_START),
+		filename_ptn%(RANGE_MAX)
+		))
